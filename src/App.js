4c8ce70485275b3f6a70945a0933cb0d0360beb7
Prashant <prashant.gyawali123@gmail.com>
2023-12-16 08:20:04 +0545

    Fixing deployment

--- src/App.js
+++ src/App.js
@@ -35,11 +35,11 @@ function App() {
         <Navbar />
         <ScrollToTop />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<Navigate to="/"/>} />
+          <Route path="*" element={<Navigate to="/home"/>} />
         </Routes>
         <Footer />
       </div>
